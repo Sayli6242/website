7e55d4f1369b8a7353aad65086054294633b65cd
Sayli Patil <www.saylipatil1603@gmail.com>
2024-07-04 20:24:23 +0530

    updates in website

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
    $('.food-slider').slick({
-      autoplay:true,
-     slidesToShow:3,
-     slidesToScroll:1,
-     prevArrow:".prev-btn",
-     nextArrow:".next-btn",
-     responsive:[
-        {
-           breakpoint:992,
-           settings:{
-            slidesToShow:2,
-           }
-        },
-        {
-         breakpoint:768,
-         settings:{
-          slidesToShow:1,
+      autoplay: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: ".prev-btn",
+      nextArrow: ".next-btn",
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+            }
          }
-      }
-     ]
+      ]
 
    });
 
-   $('.nav-trigger').click(function(){
+   $('.nav-trigger').click(function () {
       $('.site-content-wrapper').toggleClass('scaled');
    })
 });
